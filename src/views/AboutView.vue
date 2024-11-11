3252558919a77ec6d8477ab3037a612b6f7355d8
Fma <info@fmakdemir.com>
2024-11-11 00:02:57 +0000

    feat: added files and woodpecker pipeline

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>X // On click // Open link: "https://x.com/TreesforCities";</div>
+    <div>insta // On click // Open link: "https://www.instagram.com/the.tree.page/";</div>
+    <div>about: // On click // Open link: "https://iluvtreesdoyouinfo.carrd.co";</div>
   </div>
 </template>
 
